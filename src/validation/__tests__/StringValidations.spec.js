8defccf0cc73d3e624d12deea4d4110d2abf23ab
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2017-08-20 19:47:32 -0300

    Add StringValidations tests

---
+++ src/validation/__tests__/StringValidations.spec.js
@@ -0,0 +1,53 @@
+import * as Strings from '../StringValidations';
+
+import { setupTest } from '../../../test/utils';
+
+beforeEach(async () => setupTest());
+
+it('isAlpha - empty', () => {
+  expect(Strings.isAlpha()()).toMatchSnapshot();
+});
+
+it('isAlpha - valid alpha', () => {
+  expect(Strings.isAlpha()('John Doe')).toMatchSnapshot();
+});
+
+it('isAlpha - invalid alpha', () => {
+  expect(Strings.isAlpha()('John Doe #@*&')).toMatchSnapshot();
+});
+
+it('isEmail - empty', () => {
+  expect(Strings.isEmail()()).toMatchSnapshot();
+});
+
+it('isEmail - valid email', () => {
+  expect(Strings.isEmail()('john@doe.com')).toMatchSnapshot();
+});
+
+it('isEmail - invalid email', () => {
+  expect(Strings.isEmail()('fake@d.c.b')).toMatchSnapshot();
+});
+
+it('minLength - empty', () => {
+  expect(Strings.minLength(10)()).toMatchSnapshot();
+});
+
+it('minLength - valid sentence', () => {
+  expect(Strings.minLength(5)('12345')).toMatchSnapshot();
+});
+
+it('minLength - invalid sentence', () => {
+  expect(Strings.minLength(5)('123')).toMatchSnapshot();
+});
+
+it('minWords - empty', () => {
+  expect(Strings.minWords(2)()).toMatchSnapshot();
+});
+
+it('minWords - valid sentence', () => {
+  expect(Strings.minWords(2)('John Doe')).toMatchSnapshot();
+});
+
+it('minWords - invalid sentence', () => {
+  expect(Strings.minWords(2)('John')).toMatchSnapshot();
+});
